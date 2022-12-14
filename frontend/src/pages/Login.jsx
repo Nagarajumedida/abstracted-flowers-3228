ac0b77232a1edf576ad0770379e47baacb7965e6
grvsharma1994 <03041994sharma@gmail.com>
2022-12-14 23:12:34 +0530

    day-2

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import React,{useContext,useState} from 'react'
+import {Box,Input,Button,Text} from '@chakra-ui/react'
+import {useNavigate} from 'react-router-dom'
+const Login = () => {
+const navigate = useNavigate();
+const [first,setFirst] = useState(false)
+  return (
+    <div style={{height:"300px"}}>
+        <br/>
+        <Box marginLeft="60%" width="500px" boxShadow='md' display="flex" flexDirection="column" height="250px" justifyContent="space-around" alignItems="center">
+        <Text fontWeight="bold">Login/Register</Text>
+        <Input type="number" placeholder='Enter Mobile Number' width="90%" onChange={(e)=>setMobile(e.target.value)}/>
+        {first&&<Text color="red" fontSize='sm'>Mobile number is necessary</Text>}
+        <Button colorScheme='red' width="90%" onClick={()=>{
+          if(mobile.length!==0)
+            navigate("/register")
+            else
+            setFirst(true)
+          }}>PROCEED</Button>
+        </Box>
+        <br/>
+    </div>
+  )
+}
+export default Login
