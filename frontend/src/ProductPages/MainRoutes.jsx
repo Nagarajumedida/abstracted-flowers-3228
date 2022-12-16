1330f9135ecf8a7a255129e116434833ef4426cc
unknown <kumarsugam2609@gmail.com>
2022-12-16 13:40:08 +0530

    new

--- frontend/src/ProductPages/MainRoutes.jsx
+++ frontend/src/ProductPages/MainRoutes.jsx
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import EditProductPage from './EditProductPage'
 import ProductRecords from './ProductRecords'
 import SingleProductRecord from './SingleProductRecord'
+import Home from "../pages/Homepage/Homepage"
 
 const MainRoutes = () => {
   return (
     <Routes>
-         
-        <Route path='/' element={<ProductRecords/>}/>
+         <Route path='/' element={<Home/>}/>
+        <Route path='/product' element={<ProductRecords/>}/>
         <Route path="/product/:id" element={<SingleProductRecord/>}/>
         <Route path="/product/:id/edit" element={<EditProductPage/>}/>
         <Route path="*" element={<h3>Page Not Found</h3>}/>
