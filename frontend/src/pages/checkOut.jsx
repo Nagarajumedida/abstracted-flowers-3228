e9135b2e4d5832a40e00af9a61421a7bcb232a49
dilipsingh076 <101004823+dilipsingh076@users.noreply.github.com>
2022-12-18 01:49:13 +0530

    second

--- frontend/src/pages/checkOut.jsx
+++ frontend/src/pages/checkOut.jsx
@@ -1,38 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import  {useForm} from "react-hook-form";
+import "./checkOut.module.css";
 
-const Checkout = () => {
+
+
+ export const CheckOut = () => {
+  const [data, setData] = useState({});
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+    setData(values);
+  };
+
   return (
-    <div className="checkout">
+    <div>
       <h1>Checkout</h1>
-      <form>
-        <div className="form-row">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
           <label>Name</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" ref={register} />
         </div>
-        <div className="form-row">
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" name="email" ref={register} />
+        </div>
+        <div className="form-group">
           <label>Address</label>
-          <input type="text" name="address" />
+          <input type="text" name="address" ref={register} />
         </div>
-        <div className="form-row">
+        <div className="form-group">
           <label>City</label>
-          <input type="text" name="city" />
+          <input type="text" name="city" ref={register} />
         </div>
-        <div className="form-row">
+        <div className="form-group">
           <label>State</label>
-          <input type="text" name="state" />
+          <input type="text" name="state" ref={register} />
         </div>
-        <div className="form-row">
+        <div className="form-group">
           <label>Zip Code</label>
-          <input type="text" name="zip" />
+          <input type="text" name="zipcode" ref={register} />
         </div>
-        <div className="form-row">
+        <div className="form-group">
+          <label>Country</label>
+          <input type="text" name="country" ref={register} />
+        </div>
+        <div className="form-group">
           <label>Credit Card Number</label>
-          <input type="text" name="credit-card" />
+          <input type="text" name="creditCardNumber" ref={register} />
         </div>
-        <button type="submit">Submit</button>
+        <div className="form-group">
+          <label>Expiration Date</label>
+          <input type="text" name="expirationDate" ref={register} />
+        </div>
+        <div className="form-group">
+          <label>CVV</label>
+          <input type="text" name="cvv" ref={register} />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Submit" />
+        </div>
       </form>
+      {data.name ? (
+        <div>
+          <h3>Confirmation</h3>
+          <p>Name: {data.name}</p>
+          <p>Email: {data.email}</p>
+          <p>Address: {data.address}</p>
+          <p>City: {data.city}</p>
+          <p>State: {data.state}</p>
+          <p>Zip Code: {data.zipCode}</p>
+          <p>Country: {data.country}</p>
+        </div>
+      ) : null}
     </div>
   );
 };
 
-export default Checkout;
--- frontend/src/pages/paymentPage.jsx
+++ frontend/src/pages/paymentPage.jsx
@@ -1,73 +1,234 @@
-import React, { Component } from 'react'; 
+// import React, { Component } from 'react'; 
 
-class Payment extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nameOnCard: '',
-            cardNumber: '',
-            expMonth: '',
-            expYear: '',
-            cvv: ''
-        };
-    }
+// class Payment extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             nameOnCard: '',
+//             cardNumber: '',
+//             expMonth: '',
+//             expYear: '',
+//             cvv: ''
+//         };
+//     }
 
-    handleInputChange = (event) => {
-        const {name, value} = event.target;
-        this.setState({
-            [name]: value
-        });
-    }
+//     handleInputChange = (event) => {
+//         const {name, value} = event.target;
+//         this.setState({
+//             [name]: value
+//         });
+//     }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        // re-direct to another page
-    }
+//     handleSubmit = (event) => {
+//         event.preventDefault();
+//         // re-direct to another page
+//     }
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Name on Card</label>
-                    <input
-                        name="nameOnCard"
-                        type="text"
-                        value={this.state.nameOnCard}
-                        onChange={this.handleInputChange}
-                    />
-                    <label>Card Number</label>
-                    <input
-                        name="cardNumber"
-                        type="text"
-                        value={this.state.cardNumber}
-                        onChange={this.handleInputChange}
-                    />
-                    <label>Expiry Month</label>
-                    <input
-                        name="expMonth"
-                        type="text"
-                        value={this.state.expMonth}
-                        onChange={this.handleInputChange}
-                    />
-                    <label>Expiry Year</label>
-                    <input
-                        name="expYear"
-                        type="text"
-                        value={this.state.expYear}
-                        onChange={this.handleInputChange}
-                    />
-                    <label>CVV</label>
-                    <input
-                        name="cvv"
-                        type="text"
-                        value={this.state.cvv}
-                        onChange={this.handleInputChange}
-                    />
-                    <button type="submit">Submit</button>
-                </form>
+//     render() {
+//         return (
+//             <div>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <label>Name on Card</label>
+//                     <input
+//                         name="nameOnCard"
+//                         type="text"
+//                         value={this.state.nameOnCard}
+//                         onChange={this.handleInputChange}
+//                     />
+//                     <label>Card Number</label>
+//                     <input
+//                         name="cardNumber"
+//                         type="text"
+//                         value={this.state.cardNumber}
+//                         onChange={this.handleInputChange}
+//                     />
+//                     <label>Expiry Month</label>
+//                     <input
+//                         name="expMonth"
+//                         type="text"
+//                         value={this.state.expMonth}
+//                         onChange={this.handleInputChange}
+//                     />
+//                     <label>Expiry Year</label>
+//                     <input
+//                         name="expYear"
+//                         type="text"
+//                         value={this.state.expYear}
+//                         onChange={this.handleInputChange}
+//                     />
+//                     <label>CVV</label>
+//                     <input
+//                         name="cvv"
+//                         type="text"
+//                         value={this.state.cvv}
+//                         onChange={this.handleInputChange}
+//                     />
+//                     <button type="submit">Submit</button>
+//                 </form>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Payment;
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
+import './payment.css';
+
+class paymentPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      paymentMode: 'cash'
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ paymentMode: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="payment">
+        <div className="payment-container">
+          <h1>Payment</h1>
+          <form>
+            <div className="payment-mode">
+              <h3>Payment Mode</h3>
+              <div className="payment-options">
+                <div>
+                  <input
+                    type="radio"
+                    name="payment-mode"
+                    value="cash"
+                    id="cash"
+                    onChange={this.handleChange}
+                    checked={this.state.paymentMode === 'cash'}
+                  />
+                  <label htmlFor="cash">Cash</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    name="payment-mode"
+                    value="credit_card"
+                    id="credit_card"
+                    onChange={this.handleChange}
+                    checked={this.state.paymentMode === 'credit_card'}
+                  />
+                  <label htmlFor="credit_card">Credit Card</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    name="payment-mode"
+                    value="debit_card"
+                    id="debit_card"
+                    onChange={this.handleChange}
+                    checked={this.state.paymentMode === 'debit_card'}
+                  />
+                  <label htmlFor="debit_card">Debit Card</label>
+                </div>
+              </div>
             </div>
-        );
-    }
+            {this.state.paymentMode === 'cash' && (
+              <div className="cash-payment">
+                <h3>Cash Payment</h3>
+                <div>
+                  <label>
+                    Amount to be paid <span>Rs. 1000</span>
+                  </label>
+                </div>
+              </div>
+            )}
+            {this.state.paymentMode === 'credit_card' && (
+              <div className="credit-card-payment">
+                <h3>Credit Card Payment</h3>
+                <div>
+                  <label>
+                    Card Type
+                    <select>
+                      <option value="visa">Visa</option>
+                      <option value="mastercard">Mastercard</option>
+                      <option value="american_express">American Express</option>
+                    </select>
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Card Number <input type="text" placeholder="1234 5678 9101 1121" />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Name on the Card <input type="text" placeholder="John Doe" />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Expiry Date <input type="text" placeholder="MM/YY" />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    CVV <input type="text" placeholder="123" />
+                  </label>
+                </div>
+              </div>
+            )}
+            {this.state.paymentMode === 'debit_card' && (
+              <div className="debit-card-payment">
+                <h3>Debit Card Payment</h3>
+                <div>
+                  <label>
+                    Card Type
+                    <select>
+                      <option value="visa">Visa</option>
+                      <option value="mastercard">Mastercard</option>
+                      <option value="american_express">American Express</option>
+                    </select>
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Card Number <input type="text" placeholder="1234 5678 9101 1121" />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Name on the Card <input type="text" placeholder="John Doe" />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    Expiry Date <input type="text" placeholder="MM/YY" />
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    CVV <input type="text" placeholder="123" />
+                  </label>
+                </div>
+              </div>
+            )}
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Payment;
+export default paymentPage;
